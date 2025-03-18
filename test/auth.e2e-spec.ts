9dbc8c8c0b39cb28a4e2c7ec9c16070413bd59bb
Norman Aranez <araneznorman12@gmail.com>
2025-03-18 23:11:12 +0800

    add e2e testing

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a singup request', () => {
+    const email = 'e2e2@test.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201)
+      .then((res) => {
+        expect(res.body.id).toBeDefined();
+        expect(res.body.email).toEqual(email);
+      });
+  });
+});
